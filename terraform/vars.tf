4aab5ba6431f05d1540fd01cb332e32ee361e7f6
KirlikDaMirlik <kenert.piilberg@rak.ee>
2025-03-28 10:41:44 +0200

    kenert

---
+++ terraform/vars.tf
@@ -0,0 +1,20 @@
+variable "ssh_key" {
+  default = ""
+}
+variable "proxmox_host" {
+  default = "tiim1"
+}
+variable "template_id" {
+  default = "8000"
+}
+
+# variable "virtual_environment_api_token" {
+#     default = ""
+# }
+variable "proxmox_password" {
+  default = ""
+}
+
+variable "proxmox_endpoint" {
+    default = ""
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "3..1-rc6"
+    }
+  }
+}
